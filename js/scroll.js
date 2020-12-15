f96a3b64ec46abc222dc90fd78fa1c7a8732182e
Sergi <setapeses@gmail.com>
2020-12-15 23:49:15 +0100

    15/12/2020 update

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("nav").style.backgroundColor = "white";
+    } else {
+        document.getElementById("nav").style.backgroundColor = "rgba(0,0,0,0.0)";
+    }
+}
